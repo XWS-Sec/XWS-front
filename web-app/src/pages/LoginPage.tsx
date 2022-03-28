87873967236c7ec72ab99475c73e85ba5722b3d5
lukamatic <73677231+lukamatic@users.noreply.github.com>
2022-03-28 21:14:43 +0200

    Created login form

--- web-app/src/pages/LoginPage.tsx
+++ web-app/src/pages/LoginPage.tsx
@@ -1,23 +1,50 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import LoadingSpinner from '../components/common/LoadingSpinner';
 import AuthContext, { User } from '../context/auth-context';
 import localStorageUtil from '../utils/local-storage/local-storage-util';
+import { sleep } from '../utils/sleep';
 
 const LoginPage = () => {
   const authContext = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const fakeLogin = () => {
-    const user: User = {
-      accessToken: 'fake.access_token.123',
-      loggedIn: true,
-      id: 1,
-      username: 'fake_user',
-    };
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorText, setErrorText] = useState('');
+  const [fetching, setFetching] = useState(false);
 
-    localStorageUtil.setUser(user);
-    authContext.updateAuthContext(user);
-    navigate('');
+  const usernameChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setUsername(event.target.value);
+  };
+
+  const passwordChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPassword(event.target.value);
+  };
+
+  const fakeLogin = async () => {
+    setFetching(true);
+    await sleep(1000);
+    setFetching(false);
+
+    if (username === 'fake_user' && password === 'fakepass123') {
+      const user: User = {
+        accessToken: 'fake.access_token.123',
+        loggedIn: true,
+        id: 1,
+        username: 'fake_user',
+      };
+
+      localStorageUtil.setUser(user);
+      authContext.updateAuthContext(user);
+      navigate('');
+    } else {
+      setErrorText('Invalid credentials!');
+    }
   };
 
   const logIn = () => {
@@ -25,12 +52,57 @@ const LoginPage = () => {
     //TODO: implement real login
   };
 
+  const signUp = () => {
+    navigate('signup');
+  };
+
+  const onInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      logIn();
+    }
+  };
+
   return (
     <div>
-      <p>login</p>
-      <button className='btnGreenWhite' onClick={logIn}>
-        Log in
-      </button>
+      <h1 className='text-green-600 text-4xl text-center font-bold mt-20 mb-8'>
+        Welcome to Dislinkt
+      </h1>
+      <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center mb-3'>
+          <input
+            className='input p-1 mb-2 md:text-lg'
+            type='text'
+            placeholder='username'
+            name='username'
+            onChange={usernameChangeHandler}
+            onKeyDown={onInputKeyDown}
+          />
+          <input
+            className='input p-1 md:text-lg'
+            type='password'
+            placeholder='password'
+            onChange={passwordChangeHandler}
+            onKeyDown={onInputKeyDown}
+          />
+        </div>
+        {fetching ? (
+          <div className='mt-4'>
+            <LoadingSpinner />
+          </div>
+        ) : (
+          <div className='flex flex-col w-80 px-16 md:px-0 text-lg'>
+            <p className='text-red-600 text-center text' hidden={!errorText}>
+              {errorText}
+            </p>
+            <button className='btnGreenWhite my-2' onClick={logIn}>
+              Log in
+            </button>
+            <button className='btnWhiteGreen' onClick={signUp}>
+              Sign up
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };

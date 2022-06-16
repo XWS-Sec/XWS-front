8a4af942f1361355ced85dc46db3c0824db4cdf3
Petar Obradovic <ns.o.petar@gmail.com>
2022-06-16 15:52:00 +0200

    feat: Facebook SSO

--- web-app/src/components/login/LoginForm.tsx
+++ web-app/src/components/login/LoginForm.tsx
@@ -1,126 +1,143 @@
 import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { loginRequest } from '../../api/login';
+import { loginFacebookRequest, loginRequest } from '../../api/login';
 import LoadingSpinner from '../../components/common/LoadingSpinner';
-import AuthContext from '../../context/auth-context';
+import AuthContext, { User } from '../../context/auth-context';
 import { HttpStatusCode } from '../../utils/http-status-code.enum';
 import localStorageUtil from '../../utils/local-storage/local-storage.util';
+import FacebookLogin from 'react-facebook-login';
+import { getUserInfoAsync } from '../../pages/profile/profileService';
 
 const LoginForm = (props: { changeState: (value: string) => void }) => {
-  const { changeState } = props;
-  const authContext = useContext(AuthContext);
-  const navigate = useNavigate();
+	const { changeState } = props;
+	const authContext = useContext(AuthContext);
+	const navigate = useNavigate();
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-  const [errorText, setErrorText] = useState('');
+	const [errorText, setErrorText] = useState('');
 
-  const [fetching, setFetching] = useState(false);
+	const [fetching, setFetching] = useState(false);
 
-  const usernameChangeHandler = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setUsername(event.target.value);
-  };
+	const usernameChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setUsername(event.target.value);
+	};
 
-  const passwordChangeHandler = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setPassword(event.target.value);
-  };
+	const passwordChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(event.target.value);
+	};
 
-  const logIn = async () => {
-    setFetching(true);
+	const logIn = async () => {
+		setFetching(true);
 
-    const response = await loginRequest(username, password);
+		const response = await loginRequest(username, password);
 
-    switch (response.status) {
-      case HttpStatusCode.OK:
-        setErrorText('');
+		switch (response.status) {
+			case HttpStatusCode.OK:
+				setErrorText('');
 
-        const user = await response.json();
-        user.loggedIn = true;
+				const user = await response.json();
+				user.loggedIn = true;
 
-        localStorageUtil.setUser(user);
-        authContext.updateAuthContext(user);
-        navigate('');
-        break;
-      case HttpStatusCode.BAD_REQUEST:
-        setErrorText('Invalid credentials.');
-        break;
-      default:
-        alert('Unknown error occured');
-        break;
-    }
+				localStorageUtil.setUser(user);
+				authContext.updateAuthContext(user);
+				navigate('');
+				break;
+			case HttpStatusCode.BAD_REQUEST:
+				setErrorText('Invalid credentials.');
+				break;
+			default:
+				alert('Unknown error occured');
+				break;
+		}
 
-    setFetching(false);
-  };
+		setFetching(false);
+	};
 
-  const signUp = () => {
-    navigate('signup');
-  };
+	const signUp = () => {
+		navigate('signup');
+	};
 
-  const onInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      logIn();
-    }
-  };
+	const onInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === 'Enter') {
+			logIn();
+		}
+	};
 
-  const passwordlessLogin = () => {
-    navigate('passwordlessLogin');
-  };
+	const passwordlessLogin = () => {
+		navigate('passwordlessLogin');
+	};
 
-  return (
-    <div className='flex flex-col items-center'>
-      <div className='flex flex-col items-center mb-3'>
-        <input
-          className='input p-1 mb-2 md:text-lg'
-          type='text'
-          placeholder='username'
-          name='username'
-          onChange={usernameChangeHandler}
-          onKeyDown={onInputKeyDown}
-        />
-        <input
-          className='input p-1 md:text-lg'
-          type='password'
-          placeholder='password'
-          onChange={passwordChangeHandler}
-          onKeyDown={onInputKeyDown}
-        />
+	const responseFacebook = async (facebookResponse: any) => {
+		const { accessToken } = facebookResponse;
 
-        <span
-          className='cursor-pointer'
-          onClick={() => {
-            changeState('forgotPassword');
-          }}
-        >
-          Forgot password
-        </span>
-      </div>
-      {fetching ? (
-        <div className='mt-4'>
-          <LoadingSpinner />
-        </div>
-      ) : (
-        <div className='flex flex-col w-80 px-16 md:px-0 text-lg'>
-          <p className='text-red-600 text-center text' hidden={!errorText}>
-            {errorText}
-          </p>
-          <button className='btnGreenWhite my-2' onClick={logIn}>
-            Log in
-          </button>
-          <button className='btnWhiteGreen mb-2' onClick={passwordlessLogin}>
-            Log in passwordless
-          </button>
-          <button className='btnWhiteGreen' onClick={signUp}>
-            Sign up
-          </button>
-        </div>
-      )}
-    </div>
-  );
+		setFetching(true);
+
+		const response = await loginFacebookRequest(accessToken, 'Facebook');
+		switch (response.status) {
+			case HttpStatusCode.OK:
+				setErrorText('');
+				const resp = await getUserInfoAsync();
+				if (resp.status === HttpStatusCode.OK) {
+					var user = resp.data;
+					user.loggedIn = true;
+					console.log(user);
+
+					localStorageUtil.setUser(user);
+					authContext.updateAuthContext(user);
+					navigate('');
+				}
+				break;
+
+			case HttpStatusCode.BAD_REQUEST:
+				setErrorText('Invalid credentials.');
+				break;
+			default:
+				alert('Unknown error occured');
+				break;
+		}
+		setFetching(false);
+	};
+
+	return (
+		<div className='flex flex-col items-center'>
+			<div className='flex flex-col items-center mb-3'>
+				<input className='input p-1 mb-2 md:text-lg' type='text' placeholder='username' name='username' onChange={usernameChangeHandler} onKeyDown={onInputKeyDown} />
+				<input className='input p-1 md:text-lg' type='password' placeholder='password' onChange={passwordChangeHandler} onKeyDown={onInputKeyDown} />
+
+				<span
+					className='cursor-pointer'
+					onClick={() => {
+						changeState('forgotPassword');
+					}}
+				>
+					Forgot password
+				</span>
+			</div>
+			{fetching ? (
+				<div className='mt-4'>
+					<LoadingSpinner />
+				</div>
+			) : (
+				<div className='flex flex-col w-80 px-16 md:px-0 text-lg'>
+					<p className='text-red-600 text-center text' hidden={!errorText}>
+						{errorText}
+					</p>
+					<button className='btnGreenWhite my-2' onClick={logIn}>
+						Log in
+					</button>
+					<FacebookLogin appId='402600874853584' cookie={true} version='5.0' callback={responseFacebook} fields='name,email' scope='email' />
+					<button className='btnWhiteGreen mb-2' onClick={passwordlessLogin}>
+						Log in passwordless
+					</button>
+					<button className='btnWhiteGreen' onClick={signUp}>
+						Sign up
+					</button>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default LoginForm;
